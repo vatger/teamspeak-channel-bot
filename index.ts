49ab6065b3898395db2ee43edf9a72a23b2f78fd
Nikolas Görlitz <48025581+ngoerlitz@users.noreply.github.com>
2024-08-29 10:30:37 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
 import {TeamSpeak, TeamSpeakChannel} from "ts3-nodejs-library";
 import QueryProtocol = TeamSpeak.QueryProtocol;
-import {chatChannelNames, trainingChannelNames} from "./channelNames";
+import {chatChannelNames} from "./channelNames";
 import dotenv from "dotenv"
 import * as process from "process";
+import {v4} from "uuid";
 
 dotenv.config();
 
 console.log("Starting Application...");
 
+console.log(v4())
+
 const nameMap = new Map<string, string[]>();
 nameMap.set(process.env.CHAT_PARENT_ID!, chatChannelNames);
-nameMap.set(process.env.TRAINING_PARENT_ID!, trainingChannelNames);
 
 const teamspeak = new TeamSpeak({
     host: process.env.TS_HOST,
@@ -19,6 +21,7 @@ const teamspeak = new TeamSpeak({
     queryport: Number(process.env.TS_QUERYPORT),
     username: process.env.TS_USERNAME,
     password: process.env.TS_PASSWORD,
+    keepAlive: true
 });
 
 async function getSubChannels(parentID: string): Promise<TeamSpeakChannel[]> {
@@ -32,7 +35,7 @@ async function getSubChannels(parentID: string): Promise<TeamSpeakChannel[]> {
 async function getRandomChannelName(parentID: string): Promise<string> {
     const currentChannels = await getSubChannels(parentID);
     if (!nameMap.has(parentID)) {
-        return "";
+        return v4();
     }
 
     for (const s of nameMap.get(parentID)!) {
@@ -42,7 +45,8 @@ async function getRandomChannelName(parentID: string): Promise<string> {
         }
     }
 
-    return "";
+    // Generate UUID as fallback name, if all 36 names are used
+    return v4();
 }
 
 function getEmptyChannels(subChannels: TeamSpeakChannel[]): TeamSpeakChannel[] {
@@ -88,20 +92,67 @@ async function checkChannels(parentID: string) {
     const subChannels = await getSubChannels(parentID);
     const emptyChannels = getEmptyChannels(subChannels);
 
-    if (emptyChannels.length > 1 || !nameMap.has(parentID) || subChannels.length >= nameMap.get(parentID)!.length - 1) {
+    if (emptyChannels.length > 1) {
         await cleanup(subChannels, parentID);
         return;
     }
 
-    const newName = await getRandomChannelName(parentID);
-    if (emptyChannels.length == 0) {
+    // The training channels are supposed to be called "Chat 1 - n"
+    if (parentID == process.env.TRAINING_PARENT_ID) {
+        let index = 1;
+
+        // Check that the numbering is consistent (in worst case, have to rename all channels)
+        for (const channel of subChannels) {
+            if (channel.name != `Training ${index}`) {
+                try {
+                    await channel.edit({
+                        channelName: `Training ${index}`
+                    });
+                } catch (e) {
+                    console.error(`Failed to rename channel... ${e}`);
+                }
+            }
+            index++;
+        }
+
         try {
-            await teamspeak.channelCreate(`${newName}`, {
-                cpid: parentID,
-                channelFlagSemiPermanent: true
-            });
-            return;
-        } catch (e) {}
+            const newName = `Training ${index}`;
+            if (emptyChannels.length == 0) {
+                const chan = await teamspeak.channelCreate(`${newName}`, {
+                    cpid: parentID,
+                    channelFlagSemiPermanent: true,
+                });
+
+                await chan.setPerm({
+                    permname: "b_client_force_push_to_talk",
+                    permvalue: 0
+                });
+                return;
+            }
+        } catch (e) {
+            console.error(`Failed to create new channel... ${e}`);
+        }
+    }
+
+    if (parentID == process.env.CHAT_PARENT_ID) {
+        const newName = await getRandomChannelName(parentID);
+
+        if (emptyChannels.length == 0) {
+            try {
+                const chan = await teamspeak.channelCreate(`${newName}`, {
+                    cpid: parentID,
+                    channelFlagSemiPermanent: true,
+                });
+
+                await chan.setPerm({
+                    permname: "b_client_force_push_to_talk",
+                    permvalue: 0
+                });
+                return;
+            } catch (e) {
+                console.error(`Failed to create new chat channel... ${e}`)
+            }
+        }
     }
 }
 
